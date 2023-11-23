2930d47f1ee24ee7b9b6f794890a1c269a93ef5e
johnmuthua <caveomuthama@gmail.com>
2023-11-23 21:47:49 +0300

    Add sidebar for medium screens

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,23 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-const dashData = [
-  {
-    id: 1,
-    title: "Dashboard",
-  },
-];
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { FaRegCircleQuestion } from "react-icons/fa6";
+import SmSidebar from "@/components/sidebar/SmSidebar";
+import MdSidebar from "@/components/sidebar/MdSidebar";
+
 export default function Home() {
   return (
     <main>
       <div className="w-[100%] h-[100%] flex">
-        <aside className="flex w-[10%] bg-black h-[100vh]">
+        <aside className="flex w-[10%] md:w-[20%] bg-black h-[100vh] flex-col items-center pt-4">
           <div className="flex">
             <GiHamburgerMenu className="text-white" />
           </div>
+          <div className="flex mt-4">
+            <MdSidebar />
+          </div>
         </aside>
         <aside className="flex flex-col w-[90%] bg-blue-200 h-[100vh]">
-          <div className="bg-white ">
+          <div className="bg-white">
             <h1>This will be the heading of the App</h1>
           </div>
           <div className="flex w-[30%] bg-gray-300 h-[100vh]"></div>
---
+++ components/sidebar/MdSidebar.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { FaRegCircleQuestion } from "react-icons/fa6";
+const dashData = [
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: <FaChalkboardTeacher />,
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Knowledge Base",
+    icon: <FaRegCircleQuestion />,
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "Dashboard",
+    icon: <FaChalkboardTeacher />,
+    url: "/",
+  },
+  {
+    id: 4,
+    title: "Dashboard",
+    icon: <FaChalkboardTeacher />,
+    url: "/",
+  },
+];
+
+const MdSidebar = () => {
+  const data = dashData.map((item) => {
+    return (
+      <div
+        key={item.id}
+        className="my-4 text-2xl flex flex-row md:text-sm items-center"
+      >
+        {item.icon}
+        <span className="ml-2">{item.title}</span>
+      </div>
+    );
+  });
+  return <div>{data}</div>;
+};
+
+export default MdSidebar;
