04c8b4535d5e86434749fa5efe925c48b442fb7d
johnmuthua <caveomuthama@gmail.com>
2023-11-25 14:41:42 +0300

    Add styling around editor

--- components/editor/EditorPad.tsx
+++ components/editor/EditorPad.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const EditorPad = ({ note }) => {
-  return <div>{note}</div>;
+  return (
+    <div className="h-[64vh] ">
+      <textarea
+        placeholder="Enter Your Text Here"
+        className="w-full h-full focus:outline-none p-2 text-sm"
+      />
+    </div>
+  );
 };
 
 export default EditorPad;
