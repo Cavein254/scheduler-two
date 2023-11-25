0d80458503377ce5e4a71cb37de9745c1b29ad99
johnmuthua <caveomuthama@gmail.com>
2023-11-25 18:23:42 +0300

    Add editor pad

--- components/MyEditor.tsx
+++ components/MyEditor.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Editor from "./editor/Editor";
+import EditorPad from "./editor/EditorPad";
 
 type Props = {
   note: string;
 };
 
 const MyEditor = ({ note }: Props) => {
+  const [shortnote, setShortNote] = useState<string>("");
+
   return (
     <div>
       <Editor note={note} />
